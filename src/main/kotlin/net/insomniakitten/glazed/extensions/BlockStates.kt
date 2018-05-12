0faf2189d88e270c4d17b6684d47b19f2b4425fe
InsomniaKitten <admin@designdevils.co.uk>
2018-05-12 16:29:43 +0100

    Bleh

--- src/main/kotlin/net/insomniakitten/glazed/extensions/BlockStates.kt
+++ src/main/kotlin/net/insomniakitten/glazed/extensions/BlockStates.kt
@@ -24,36 +24,25 @@ import net.minecraft.block.state.IBlockState
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
 
-operator fun IBlockState.invoke(access: IBlockAccess, pos: BlockPos): IBlockState {
-    return getActualState(access, pos)
-}
+operator fun IBlockState.invoke(access: IBlockAccess, pos: BlockPos): IBlockState = getActualState(access, pos)
 
 operator fun <V : Comparable<V>, K : IProperty<V>> K.invoke(value: V): Pair<K, V> = this to value
 
-operator fun <V : Comparable<V>> Block.contains(property: IProperty<V>): Boolean {
-    return property in blockState.properties
-}
+operator fun <V : Comparable<V>> IProperty<V>.contains(value: V) = value in allowedValues
 
-operator fun <V : Comparable<V>> IBlockState.contains(property: IProperty<V>): Boolean {
-    return property in propertyKeys
-}
+operator fun <V : Comparable<V>> IBlockState.contains(property: IProperty<V>) = property in propertyKeys
 
-operator fun <V : Comparable<V>> IBlockState.get(property: IProperty<V>): V {
-    return getValue(property)
-}
+operator fun <V : Comparable<V>> Block.contains(property: IProperty<V>) = property in blockState.properties
 
-operator fun <V : Comparable<V>> IBlockState.plus(entry: Pair<IProperty<V>, V>): IBlockState {
-    return withProperty(entry.first, entry.second)
-}
+operator fun <V : Comparable<V>> IBlockState.get(property: IProperty<V>): V = getValue(property)
 
-infix fun <V : Comparable<V>> IProperty<V>.from(state: IBlockState): V {
-    return state.getValue(this)
-}
+operator fun <V : Comparable<V>> IBlockState.plus(entry: Pair<IProperty<V>, V>): IBlockState =
+        withProperty(entry.first, entry.second)
 
-infix fun <V : Comparable<V>> IBlockState.with(entry: Pair<IProperty<V>, V>): IBlockState {
-    return withProperty(entry.first, entry.second)
-}
+infix fun <V : Comparable<V>> IProperty<V>.from(state: IBlockState): V = state.getValue(this)
 
-infix fun <V : Comparable<V>> IBlockState.cycle(property: IProperty<V>): IBlockState {
-    return cycleProperty(property)
-}
+infix fun <V : Comparable<V>> IBlockState.with(entry: Pair<IProperty<V>, V>): IBlockState =
+        withProperty(entry.first, entry.second)
+
+infix fun <V : Comparable<V>> IBlockState.cycle(property: IProperty<V>): IBlockState =
+        cycleProperty(property)
